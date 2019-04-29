b9dcd19ebbc99d5c3eaaa83e2d26c29a41472fee
bamless <bamlessnty5@gmail.com>
2019-04-29 10:06:20 +0200

    Incremented version

--- src/include/blang.h
+++ src/include/blang.h
@@ -6,10 +6,10 @@
 #include <stdlib.h>
 
 #define BLANG_VERSION_MAJOR 0
-#define BLANG_VERSION_MINOR 2
-#define BLANG_VERSION_PATCH 3
+#define BLANG_VERSION_MINOR 3
+#define BLANG_VERSION_PATCH 0
 
-#define BLANG_VERSION_STRING "0.2.3"
+#define BLANG_VERSION_STRING "0.3.0"
 
 #define BLANG_VERSION                                                                              \
     (BLANG_VERSION_MAJOR * 100000 + BLANG_VERSION_MINOR * 1000 + BLANG_VERSION_PATCH)
