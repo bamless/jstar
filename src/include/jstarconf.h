cd34ea60abcfc46a9f5abc9108f25a9ea7c774e4
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2019-08-26 11:50:50 +0200

    Increase version

--- src/include/jstarconf.h
+++ src/include/jstarconf.h
@@ -4,8 +4,8 @@
 // Version
 #define JSTAR_VERSION_MAJOR 0
 #define JSTAR_VERSION_MINOR 5
-#define JSTAR_VERSION_PATCH 1
-#define JSTAR_VERSION_STRING "0.5.1"
+#define JSTAR_VERSION_PATCH 2
+#define JSTAR_VERSION_STRING "0.5.2"
 
 // Increasing version number, used for range checking
 #define JSTAR_VERSION \
