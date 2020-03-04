91d1c98364f3d5c52e736d31b22d0f6d50de4645
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2020-03-04 16:47:03 +0100

    Incremented version

--- include/jstarconf.h
+++ include/jstarconf.h
@@ -4,8 +4,8 @@
 // Version
 #define JSTAR_VERSION_MAJOR 0
 #define JSTAR_VERSION_MINOR 9
-#define JSTAR_VERSION_PATCH 13
-#define JSTAR_VERSION_STRING "0.9.13"
+#define JSTAR_VERSION_PATCH 14
+#define JSTAR_VERSION_STRING "0.9.14"
 
 // Increasing version number, used for range checking
 #define JSTAR_VERSION \
