03dd5633877f2df9e89023dad94f7ab14f987fa5
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2020-04-02 00:31:40 +0200

    Now import lib gets installed properly on Windows

--- jstar/include/jstarconf.h
+++ jstar/include/jstarconf.h
@@ -16,7 +16,7 @@
 #define JSTAR_PLATFORM "Linux"
 
 // Options
-#define USE_COMPUTED_GOTOS
+/* #undef USE_COMPUTED_GOTOS */
 #define NAN_TAGGING
 /* #undef DBG_PRINT_EXEC */
 /* #undef DBG_PRINT_GC */
