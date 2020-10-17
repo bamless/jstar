8cf68e8fb423b092122fde5921f63674c6533271
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2020-10-17 22:49:46 +0200

    CMake restructuring WIP

---
+++ cmake/JStarConfig.cmake
@@ -0,0 +1,4 @@
+include(CMakeFindDependencyMacro)
+include("${CMAKE_CURRENT_LIST_DIR}/JStarConfigVersion.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/JStarTargets.cmake")
+message(STATUS "Found J* version ${PACKAGE_VERSION}")
