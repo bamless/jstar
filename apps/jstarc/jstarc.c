43b26aed7e3e62a98a876cbb5fc9e3055d04394f
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2020-12-01 01:03:17 +0100

    jstarc.c: added cwalk library and compiling directoriess WIP

--- apps/jstarc/jstarc.c
+++ apps/jstarc/jstarc.c
@@ -1,4 +1,7 @@
 #include <argparse.h>
+#include <assert.h>
+#include <cwalk.h>
+#include <dirent.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,36 +10,46 @@
 #include "jstar/jstar.h"
 
 typedef struct Options {
-    char* input;
-    char* out;
+    char *input, *output;
 } Options;
+
+static JStarVM* vm;
+
+static void initVM() {
+    JStarConf conf = jsrGetConf();
+    vm = jsrNewVM(&conf);
+}
+
+static void exitFree(int code) {
+    jsrFreeVM(vm);
+    exit(code);
+}
 
 Options parseArguments(int argc, char** argv) {
     Options opts = {0};
 
     static const char* const usage[] = {
-        "jstarc [options] file out",
+        "jstarc [options] file",
         NULL,
     };
 
     struct argparse_option options[] = {
         OPT_HELP(),
+        OPT_STRING('o', "output", &opts.output, "Output file", 0, 0, 0),
         OPT_END(),
     };
 
     struct argparse argparse;
     argparse_init(&argparse, options, usage, 0);
-    argparse_describe(&argparse, "jstarc: compile J* source files to bytecode", NULL);
-
+    argparse_describe(&argparse, "jstarc compiles J* source files to bytecode", NULL);
     int nonOpts = argparse_parse(&argparse, argc, (const char**)argv);
 
-    if(nonOpts != 2) {
+    if(nonOpts != 1) {
         argparse_usage(&argparse);
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
 
     opts.input = argv[0];
-    opts.out = argv[1];
     return opts;
 }
 
@@ -48,36 +61,70 @@ static bool writeToFile(const JStarBuffer* buf, const char* path) {
     return true;
 }
 
-int main(int argc, char** argv) {
-    Options opts = parseArguments(argc, argv);
+static bool isDirectory(const char* path) {
+    DIR* d = opendir(path);
+    if(d != NULL) {
+        if(closedir(d)) exitFree(errno);
+        return true;
+    }
+    return false;
+}
 
-    JStarConf conf = jsrGetConf();
-    JStarVM* vm = jsrNewVM(&conf);
-
+static void compileFile(const char* path, const char* out) {
     JStarBuffer src;
-    if(!jsrReadFile(vm, opts.input, &src)) {
-        fprintf(stderr, "Cannot open input file %s: %s\n", opts.input, strerror(errno));
-        jsrFreeVM(vm);
-        exit(EXIT_FAILURE);
+    if(!jsrReadFile(vm, path, &src)) {
+        fprintf(stderr, "Cannot open input file %s: %s\n", path, strerror(errno));
+        exitFree(EXIT_FAILURE);
     }
 
     JStarBuffer compiled;
-    JStarResult res = jsrCompileCode(vm, opts.input, src.data, &compiled);
+    JStarResult res = jsrCompileCode(vm, path, src.data, &compiled);
     if(res != JSR_SUCCESS) {
-        fprintf(stderr, "Error compiling file %s\n", opts.input);
+        fprintf(stderr, "Error compiling file %s\n", path);
         jsrBufferFree(&src);
-        jsrFreeVM(vm);
-        exit(EXIT_FAILURE);
+        exitFree(EXIT_FAILURE);
     }
 
     jsrBufferFree(&src);
 
-    if(!writeToFile(&compiled, opts.out)) {
-        fprintf(stderr, "Failed to write compiled file of %s: %s\n", opts.input, strerror(errno));
-        jsrBufferFree(&compiled);
-        jsrFreeVM(vm);
-        exit(EXIT_FAILURE);
+    char outPath[FILENAME_MAX] = {0};
+    if(out) {
+        strncat(outPath, out, sizeof(outPath));
+    } else {
+        cwk_path_change_extension(path, "jsc", outPath, sizeof(outPath));
     }
 
-    jsrFreeVM(vm);
+    if(!writeToFile(&compiled, outPath)) {
+        fprintf(stderr, "Failed to write compiled file of %s: %s\n", path, strerror(errno));
+        jsrBufferFree(&compiled);
+        exitFree(EXIT_FAILURE);
+    }
+}
+
+static void compileDirectory(const char* path, const char* out) {
+    printf("Compiling to direcoty WIP\n");
+    return;
+
+    DIR* dir = opendir(path);
+    assert(dir);
+
+    struct dirent* dirent = readdir(dir);
+    if(dirent == NULL) {
+        fprintf(stderr, "Cannot read directory %s: %s\n", path, strerror(errno));
+        exitFree(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char** argv) {
+    Options opts = parseArguments(argc, argv);
+
+    initVM();
+
+    if(isDirectory(opts.input)) {
+        compileDirectory(opts.input, opts.output);
+    } else {
+        compileFile(opts.input, opts.output);
+    }
+
+    exitFree(EXIT_SUCCESS);
 }
