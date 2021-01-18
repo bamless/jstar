142d71d8b6893189dec093b4d309f99e1d252e26
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2021-01-18 01:02:30 +0100

    value.c: c99 compliant tagged union

--- src/value.h
+++ src/value.h
@@ -130,7 +130,7 @@ typedef struct {
         double num;
         void* handle;
         Obj* obj;
-    };
+    } as;
 } Value;
 
     // clang-format off
@@ -142,18 +142,18 @@ typedef struct {
 #define IS_NULL(val)   ((val).type == VAL_NULL)
 #define IS_INT(val)    valueIsInt(val)
 
-#define AS_HANDLE(val) ((val).handle)
-#define AS_BOOL(val)   ((val).boolean)
-#define AS_NUM(val)    ((val).num)
-#define AS_OBJ(val)    ((val).obj)
+#define AS_HANDLE(val) ((val).as.handle)
+#define AS_BOOL(val)   ((val).as.boolean)
+#define AS_NUM(val)    ((val).as.num)
+#define AS_OBJ(val)    ((val).as.obj)
 
-#define HANDLE_VAL(h) ((Value){VAL_HANDLE, {.handle = h}})
-#define NUM_VAL(n)    ((Value){VAL_NUM, {.num = n}})
-#define BOOL_VAL(b)   ((Value){VAL_BOOL, {.boolean = b}})
-#define OBJ_VAL(val)  ((Value){VAL_OBJ, {.obj = (Obj*)val}})
-#define TRUE_VAL      ((Value){VAL_BOOL, {.boolean = true}})
-#define FALSE_VAL     ((Value){VAL_BOOL, {.boolean = false}})
-#define NULL_VAL      ((Value){VAL_NULL, {.num = 0}})
+#define HANDLE_VAL(h) ((Value){VAL_HANDLE, .as = {.handle = h}})
+#define NUM_VAL(n)    ((Value){VAL_NUM, .as = {.num = n}})
+#define BOOL_VAL(b)   ((Value){VAL_BOOL, .as = {.boolean = b}})
+#define OBJ_VAL(val)  ((Value){VAL_OBJ, .as = {.obj = (Obj*)val}})
+#define TRUE_VAL      ((Value){VAL_BOOL, .as = {.boolean = true}})
+#define FALSE_VAL     ((Value){VAL_BOOL, .as = {.boolean = false}})
+#define NULL_VAL      ((Value){VAL_NULL, .as = {.num = 0}})
 
 // clang-format on
 
@@ -162,13 +162,13 @@ inline bool valueEquals(Value v1, Value v2) {
 
     switch(v1.type) {
     case VAL_NUM:
-        return v1.num == v2.num;
+        return v1.as.num == v2.as.num;
     case VAL_BOOL:
-        return v1.boolean == v2.boolean;
+        return v1.as.boolean == v2.as.boolean;
     case VAL_OBJ:
-        return v1.obj == v2.obj;
+        return v1.as.obj == v2.as.obj;
     case VAL_HANDLE:
-        return v1.handle == v2.handle;
+        return v1.as.handle == v2.as.handle;
     case VAL_NULL:
         return true;
     }
