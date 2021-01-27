2773303ed67eb3cc4e3fe7a702d1fbfd7b0320c4
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2021-01-27 15:50:16 +0100

    const.h: comments

--- src/const.h
+++ src/const.h
@@ -13,7 +13,7 @@
 #define INIT_GC         (1024 * 1024 * 20)             // 10MiB - First GC collection point
 #define HEAP_GROW_RATE  2                              // The heap growing rate
 #define HANDLER_MAX     10                             // Max number of try-excepts for a frame
-#define SUPER_SLOT      0  // Constant slot holding the method's super-class
+#define SUPER_SLOT      0                              // Constant holding the method's super-class
 
 // -----------------------------------------------------------------------------
 // COMPILER CONSTANTS
