4cfde1a0121b429d50bd7e6016e67aa69707cc9c
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2021-01-30 02:54:34 +0100

    util.h: macros refactoring

--- src/util.h
+++ src/util.h
@@ -7,13 +7,11 @@
 
 #include "jstarconf.h"
 
-// Reinterprets the value `v` from type F to type T
+// Reinterprets the bits of the value `v` from type F to type T
 #define REINTERPRET_CAST(F, T, v) ((union {F f; T t;}){.f = (v)}.t)
 
-// -----------------------------------------------------------------------------
-// MACROS TO COMPUTE BASE 10 LENGHT OF INTEGERS
-// -----------------------------------------------------------------------------
-
+// Compute the approximate maximal length of an integral type in base 10
+// The computed value is an integer constant in order to permit stack buffer allocation
 #define STRLEN_FOR_INT(t)      (((t)-1 < 0) ? STRLEN_FOR_SIGNED(t) : STRLEN_FOR_UNSIGNED(t))
 #define STRLEN_FOR_SIGNED(t)   (STRLEN_FOR_UNSIGNED(t) + 1)
 #define STRLEN_FOR_UNSIGNED(t) (((((sizeof(t) * CHAR_BIT)) * 1233) >> 12) + 1)
@@ -56,24 +54,19 @@ static inline size_t roundUp(size_t num, size_t multiple) {
 #ifndef NDEBUG
     #include <stdio.h>
 
-    #define ASSERT(cond, msg)                                                                     \
-        do {                                                                                      \
-            if(!(cond)) {                                                                         \
-                fprintf(stderr, "%s [line: %d] %s(): assertion failed: %s\n", __FILE__, __LINE__, \
-                        __func__, msg);                                                           \
-                abort();                                                                          \
-            }                                                                                     \
-        } while(0)
+    #define ASSERT(cond, msg)                                                                    \
+        ((cond) ?                                                                                \
+         ((void)0) :                                                                             \
+         (fprintf(stderr, "%s [line:%d] in %s(): %s failed: %s\n", __FILE__, __LINE__, __func__, \
+                  #cond, msg), abort()))
 
-    #define UNREACHABLE()                                                                         \
-        do {                                                                                      \
-            fprintf(stderr, "%s [line:%d] %s(): reached unreachable code.\n", __FILE__, __LINE__, \
-                    __func__);                                                                    \
-            abort();                                                                              \
-        } while(0)
+    #define UNREACHABLE() \
+        (fprintf(stderr, "%s [line:%d] in %s(): Reached unreachable code.\n", __FILE__, __LINE__, \
+                 __func__), abort())
+
 #else
-    #define ASSERT(cond, msg)
-    #define UNREACHABLE()
+    #define ASSERT(cond, msg) ((void)0)
+    #define UNREACHABLE()     ((void)0)
 #endif
 
 #endif
