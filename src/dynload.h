8dee79bdf08477df3aec99708af22f9a452c36a6
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2021-02-02 17:34:05 +0100

    dynload is now header-only

--- src/dynload.h
+++ src/dynload.h
@@ -1,8 +1,22 @@
 #ifndef DYNLOAD_H
 #define DYNLOAD_H
 
-void* dynload(const char* path);
-void dynfree(void* handle);
-void* dynsim(void* handle, const char* symbol);
+#include "jstarconf.h"
+
+#if defined(JSTAR_POSIX)
+    #include <dlfcn.h>
+    #define dynload(path)          dlopen(path, RTLD_NOW)
+    #define dynfree(handle)        dlclose(handle)
+    #define dynsim(handle, symbol) dlsym(handle, symbol)
+#elif defined(JSTAR_WINDOWS)
+    #include <Windows.h>
+    #define dynload(path)          LoadLibrary(path)
+    #define dynfree(handle)        FreeLibrary(handle)
+    #define dynsim(handle, symbol) GetProcAddress(handle, symbol)
+#else
+    #define dynload(path)          ((void*)0)
+    #define dynfree(handle)        ((void)0)
+    #define dynsim(handle, symbol) ((void*)0)
+#endif
 
 #endif
