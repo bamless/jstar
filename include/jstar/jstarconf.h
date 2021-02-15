7cc73c4afa35ebd0f86835ac71c05e70a6dff167
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2021-02-15 17:52:34 +0100

    Increase version

--- include/jstar/jstarconf.h
+++ include/jstar/jstarconf.h
@@ -3,9 +3,9 @@
 
 // Version
 #define JSTAR_VERSION_MAJOR  1
-#define JSTAR_VERSION_MINOR  5
+#define JSTAR_VERSION_MINOR  6
 #define JSTAR_VERSION_PATCH  0
-#define JSTAR_VERSION_STRING "1.5.0"
+#define JSTAR_VERSION_STRING "1.6.0"
 
 // Increasing version number, used for range checking
 #define JSTAR_VERSION \
