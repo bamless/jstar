c3574a7f40430eb158b7c195a8ea4beafed26e35
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2021-07-27 17:41:54 +0200

    highliter.c: refactoring

--- apps/jstar/highlighter.c
+++ apps/jstar/highlighter.c
@@ -13,8 +13,8 @@
 #define IDENTIFIER_CALL_COLOR REPLXX_COLOR_YELLOW
 
 static const ReplxxColor theme[TOK_EOF] = {
-// Keywords
-#define KEYWORD_COLOR REPLXX_COLOR_BLUE
+    // Keywords
+    #define KEYWORD_COLOR REPLXX_COLOR_BLUE
     [TOK_AND] = KEYWORD_COLOR,
     [TOK_OR] = KEYWORD_COLOR,
     [TOK_CLASS] = KEYWORD_COLOR,
@@ -40,17 +40,17 @@ static const ReplxxColor theme[TOK_EOF] = {
     [TOK_CONTINUE] = KEYWORD_COLOR,
     [TOK_BREAK] = KEYWORD_COLOR,
 
-// `this` and `super` keywords
-#define METHOD_KEYWORD_COLOR REPLXX_COLOR_BLUE
+    // `this` and `super` keywords
+    #define METHOD_KEYWORD_COLOR REPLXX_COLOR_BLUE
     [TOK_SUPER] = METHOD_KEYWORD_COLOR,
 
-// Storage keywords
-#define STORAGE_KEYWORD_COLOR REPLXX_COLOR_BLUE
+    // Storage keywords
+    #define STORAGE_KEYWORD_COLOR REPLXX_COLOR_BLUE
     [TOK_VAR] = STORAGE_KEYWORD_COLOR,
     [TOK_STATIC] = STORAGE_KEYWORD_COLOR,
 
-// Punctuation
-#define PUNCTUATION_COLOR REPLXX_COLOR_GRAY
+    // Punctuation
+    #define PUNCTUATION_COLOR REPLXX_COLOR_GRAY
     [TOK_SEMICOLON] = PUNCTUATION_COLOR,
     [TOK_PIPE] = PUNCTUATION_COLOR,
     [TOK_LPAREN] = PUNCTUATION_COLOR,
