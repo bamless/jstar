c9a0511e368935a86b8a86e42ac677345044f230
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2021-10-01 23:37:30 +0200

    Refactoring

--- src/compiler.h
+++ src/compiler.h
@@ -3,15 +3,15 @@
 
 #include <stdint.h>
 
+#include "const.h"
 #include "jstar.h"
 #include "object.h"
 #include "parse/ast.h"
 
+#define CTOR_STR        "new"        // Special method name that signals a constructor
 #define MAX_TRY_DEPTH   HANDLER_MAX  // Max depth of nested trys
 #define MAX_LOCALS      UINT8_MAX    // At most 255 local vars per frame
 #define MAX_INLINE_ARGS 10           // Max number of inline arguments for function call
-
-#define CTOR_STR "new"  // Special method name that signals a constructor
 
 typedef struct Compiler Compiler;
 
