bd759d76e87046d1d33bf495b26d0fe6568cd707
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2023-05-29 00:05:40 +0200

    Formatting

--- src/parse/ast.c
+++ src/parse/ast.c
@@ -256,8 +256,7 @@ static JStarStmt* newStmt(int line, JStarStmtType type) {
 
 static JStarStmt* newDecl(int line, JStarStmtType type) {
     ASSERT(type == JSR_VARDECL || type == JSR_FUNCDECL || type == JSR_CLASSDECL ||
-               type == JSR_NATIVEDECL,
-           "Not a declaration");
+           type == JSR_NATIVEDECL, "Not a declaration");
     JStarStmt* s = newStmt(line, type);
     s->as.decl.isStatic = false;
     s->as.decl.decorators = vecNew();
@@ -409,8 +408,7 @@ JStarStmt* jsrBreakStmt(int line) {
 static void freeDeclaration(JStarStmt* s) {
     JStarStmtType type = s->type;
     ASSERT(type == JSR_VARDECL || type == JSR_FUNCDECL || type == JSR_CLASSDECL ||
-               type == JSR_NATIVEDECL,
-           "Not a declaration");
+           type == JSR_NATIVEDECL, "Not a declaration");
     (void)type;
 
     vecForeach(JStarExpr * *e, s->as.decl.decorators) {
